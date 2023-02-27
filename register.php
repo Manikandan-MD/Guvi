51442c98c6d33cb26ac80edcd6a7336a227b5589
Manikandan-MD <102610350+Manikandan-MD@users.noreply.github.com>
2023-02-27 17:48:15 +0530

    Add files via upload

---
+++ register.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Registeration from Php</title>
+</head>
+<body style="background-color:  yellow;font-family: sans-serif;">
+
+<h1>Registeration Form</h1>
+
+<p style="text-align: center;">
+    <label>UserName</label>
+    <input type="text" name="username">
+</p>
+    
+</body>
+</html>
